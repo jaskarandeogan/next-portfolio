f816d3ae31ec0757f37420c630bd9bddd568edea
jaskarandeogan <jaskaran2k15@gmail.com>
2023-10-07 11:03:09 -0700

    add social links

--- src/components/SocialIcons.tsx
+++ src/components/SocialIcons.tsx
@@ -7,10 +7,32 @@ import { FaLinkedinIn, FaInstagram, FaGithub } from 'react-icons/fa'
 const SocialIcons = () => {
     return (
         <section className={classNames('flex', 'gap-8', 'items-center')}>
-            <AiOutlineTwitter className={classNames('h-10 w-10 text-primary-0 hover:cursor-pointer hover:-translate-y-1 transition-all')}/>
-            <FaLinkedinIn className={classNames('h-10 w-10 text-primary-0 hover:cursor-pointer hover:-translate-y-1 transition-all')}/>
-            <FaInstagram className={classNames('h-10 w-10 text-primary-0 hover:cursor-pointer hover:-translate-y-1 transition-all')}/>
-            <FaGithub className={classNames('h-10 w-10 text-primary-0 hover:cursor-pointer hover:-translate-y-1 transition-all')}/>
+            <AiOutlineTwitter className={classNames('h-10 w-10 text-primary-0 hover:cursor-pointer hover:-translate-y-1 transition-all')}
+                onClick={() => {
+                    window.open('https://twitter.com/DeoganJaskaran')
+                }}
+            />
+            <FaLinkedinIn className={classNames('h-10 w-10 text-primary-0 hover:cursor-pointer hover:-translate-y-1 transition-all')}
+                onClick={
+                    () => {
+                        window.open('www.linkedin.com/in/jaskaran-deogan')
+                    }
+                }
+            />
+            <FaInstagram className={classNames('h-10 w-10 text-primary-0 hover:cursor-pointer hover:-translate-y-1 transition-all')}
+                onClick={
+                    () => {
+                        window.open('https://www.instagram.com/jskrndeogxn/')
+                    }
+                }
+            />
+            <FaGithub className={classNames('h-10 w-10 text-primary-0 hover:cursor-pointer hover:-translate-y-1 transition-all')}
+                onClick={
+                    () => {
+                        window.open('https://github.com/jaskarandeogan')
+                    }
+                }
+            />
         </section>
     )
 }
