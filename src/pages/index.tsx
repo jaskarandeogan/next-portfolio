8a09addf56abe796dc5fb594725311c8eeb3bfc7
jaskarandeogan <jaskaran2k15@gmail.com>
2023-10-22 01:55:06 -0700

    bypass dev env

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,8 @@
-import { useEffect } from "react";
-import { useRouter } from "next/router";
 import Home from "@/layouts/Home";
 
 
 export default function Index() {
-  const { pathname, replace } = useRouter();
+  
 
-  const isDevenv = process.env.NEXT_PUBLIC_ENV === "development";
-
-  useEffect(() => {
-    if (pathname === "/" && !isDevenv ) {
-      replace("/");
-    }
-  }, [isDevenv, pathname, replace]);
-
-  if(isDevenv) return <Home />
-  else return null;
+  return <Home />;
 }
