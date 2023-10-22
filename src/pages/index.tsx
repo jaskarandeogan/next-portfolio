36b87fd5f9446d47fb7e4e0c2301ab4adf35df57
jaskarandeogan <jaskaran2k15@gmail.com>
2023-10-22 01:52:31 -0700

    bypass development

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,7 +10,7 @@ export default function Index() {
 
   useEffect(() => {
     if (pathname === "/" && !isDevenv ) {
-      replace("/comingsoon");
+      replace("/");
     }
   }, [isDevenv, pathname, replace]);
 
