8e243faa773210472e614ad4641290a7bac98ece
jaskarandeogan <jaskaran2k15@gmail.com>
2023-11-24 19:49:11 -0800

    change profile

--- src/components/ProfileSection.tsx
+++ src/components/ProfileSection.tsx
@@ -11,8 +11,8 @@ const ProfileSection = () => {
             'gap-6',
             'lg:items-center'
         )}>
-            <Image src={"/images/Profile.jpg" || "https://firebasestorage.googleapis.com/v0/b/portfolio-e70ab.appspot.com/o/Profile.png?alt=media&token=f33f23e4-4c80-43ef-98f4-00af768103ea&_gl=1*16ykvvs*_ga*NzU2NzY3NjI5LjE2ODY1MzQ5MDI.*_ga_CW55HF8NVT*MTY5Nzk2NjAzNC40LjEuMTY5Nzk2NjYzNy40MS4wLjA."} alt="Picture of the author" width={133} height={133} className="hidden md:block rounded-full" />
-            <Image src={"/images/Profile.jpg" || "https://firebasestorage.googleapis.com/v0/b/portfolio-e70ab.appspot.com/o/Profile-transformed.png?alt=media&token=28975845-aeb4-4a66-b66f-4b5a3e014860&_gl=1*14qwgkm*_ga*NzU2NzY3NjI5LjE2ODY1MzQ5MDI.*_ga_CW55HF8NVT*MTY5OTEyMzkxOC41LjEuMTY5OTEyNDA5NC41Ni4wLjA."} alt="Picture of the author" width={200} height={200} className="m-auto md:hidden rounded-full" />
+            <Image src={"/images/Profile.png" || "https://firebasestorage.googleapis.com/v0/b/portfolio-e70ab.appspot.com/o/Profile.png?alt=media&token=f33f23e4-4c80-43ef-98f4-00af768103ea&_gl=1*16ykvvs*_ga*NzU2NzY3NjI5LjE2ODY1MzQ5MDI.*_ga_CW55HF8NVT*MTY5Nzk2NjAzNC40LjEuMTY5Nzk2NjYzNy40MS4wLjA."} alt="Picture of the author" width={133} height={133} className="hidden md:block rounded-full" />
+            <Image src={"/images/Profile.png" || "https://firebasestorage.googleapis.com/v0/b/portfolio-e70ab.appspot.com/o/Profile-transformed.png?alt=media&token=28975845-aeb4-4a66-b66f-4b5a3e014860&_gl=1*14qwgkm*_ga*NzU2NzY3NjI5LjE2ODY1MzQ5MDI.*_ga_CW55HF8NVT*MTY5OTEyMzkxOC41LjEuMTY5OTEyNDA5NC41Ni4wLjA."} alt="Picture of the author" width={200} height={200} className="m-auto md:hidden rounded-full" />
             <div className="flex flex-col gap-1 lg:gap-3">
                 <h2 className={classNames('text-primary-0',
                     'text-[2rem]',
