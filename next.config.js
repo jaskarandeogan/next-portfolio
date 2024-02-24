274a8eaca040689432146f578df0dcce126cbe2e
Jaskaran Deogan <jaskaran.deogan@payfelix.com>
2024-02-24 08:38:22 -0800

    add analytics

--- next.config.js
+++ next.config.js
@@ -4,6 +4,9 @@ const nextConfig = {
   images: {
     domains: ['firebasestorage.googleapis.com'],
   },
+  analytics: {
+    googleAnalyticsId: 'G-H2JY9SK2FK',
+  },
 }
 
 module.exports = nextConfig
