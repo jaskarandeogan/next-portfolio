31b7adca0ad9b647ce7a6fa38517f8eb869e60d0
jaskarandeogan <jaskaran2k15@gmail.com>
2024-05-26 21:37:03 -0700

    add og tags

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,12 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head >
+        <meta property="og:title" content="Jaskaran's Space" />
+        <meta property="og:description" content="I aim to create the next generation of business portfolios and enhance web experiences." />
+        <meta name="twitter:title" content="Jaskaran's Space" />
+        <meta name="twitter:description" content="I aim to create the next generation of business portfolios and enhance web experiences." />
+      </Head>
       <body>
         <Main />
         <NextScript />
